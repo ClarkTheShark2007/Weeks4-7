95657876c74948501ae641d889e51885b77d33c7
Clark <osborneclark5@gmail.com>
2026-02-12 15:30:53 -0500

    Completed the final spell and made a little win statue for using the base 5.

--- Assets/ToyAssigment/Scripts/SpellSpawner.cs
+++ Assets/ToyAssigment/Scripts/SpellSpawner.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class SpellSpawner : MonoBehaviour
@@ -7,17 +8,24 @@ public class SpellSpawner : MonoBehaviour
     public GameObject ThunderstormSpell;
     public GameObject EarthSpell;
     public GameObject GhastSpell;
+    public GameObject resetSpell;
+    public GameObject winScreen;
+    int totalSpellsCasted;
+    public TextMeshProUGUI castText;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        castText.text = totalSpellsCasted.ToString();
+
+        if(totalSpellsCasted >= 5)
+        {
+            winScreen.SetActive(true);
+        } else
+        {
+            winScreen.SetActive(false);
+        }
     }
 
     public void CastSpell(int spellID)
@@ -26,30 +34,36 @@ public class SpellSpawner : MonoBehaviour
         {
             Debug.Log("Fire Spell Casted");
             Instantiate(FireSpell);
+            totalSpellsCasted++;
         }
         else if (spellID == 2)
         {
             Debug.Log("Ice Spell Casted");
             Instantiate(IceSpell);
+            totalSpellsCasted++;
         }
         else if(spellID == 3)
         {
             Debug.Log("Thunder Spell Casted");
             Instantiate(ThunderstormSpell);
+            totalSpellsCasted++;
         }
         else if(spellID == 4)
         {
             Debug.Log("Earth Spell Casted");
             Instantiate(EarthSpell);
+            totalSpellsCasted++;
         }
         else if(spellID == 5)
         {
             Debug.Log("Ghast Spell Casted");
             Instantiate(GhastSpell);
+            totalSpellsCasted++;
         }
         else if(spellID == 6)
         {
-            
+            Instantiate(resetSpell);
+            totalSpellsCasted = 0;
         }
     }
 }
